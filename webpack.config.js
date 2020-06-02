00e06222ca45c61016fec4a62ba4fa02852339d4
Andrea Panattoni <a.panattoni@list-group.com>
2020-06-02 10:56:36 +0200

    HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -7,4 +8,11 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'build'),
   },
+  plugins: [
+
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'src/index.html',
+    }),
+  ]
 };
